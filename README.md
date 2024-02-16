f93a94679232c61efde66e7844c8a4d3b2ba14aa
yjw5344 <yjw5344@hanmail.net>
2024-02-17 01:12:08 +0900

    markdown 작성 및 크롤러 완료

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# kia_price
-kia price compare
+# 기아 차량 가격 크롤러
+
+## 개요
+기아 DCS 컨피규레이터와 기아닷컴 사이의 가격 비교를 위해 기아닷컴에서 차량 컨피규레이션에 따른 가격 정보를 엑셀로 저장하는 크롤러(Selenium)
+
+## 사용법
+```bash
+
+pip install selenium
+pip install webdriver-manager
+pip install configparser
+pip install openpyxl
+
+python priceCrawling.py ## 해당 파일의 폴더 경로에서 파이썬 코드 실행
+```
+
+__`result/` 경로에서 excel 파일로 추출 결과 확인 가능__
+
+## 차량 추가 방법
+
+1. `setting.ini`에 차량 필수 정보를 입력
+2. `setting.ini`에서 추가한 차량에 대해 `priceCrawling.py`의 코드 추가
+
+### 1. setting.ini에 차량 필수 정보 입력
+
+```ini
+[Default]
+RESULTPAGE_LOADTIME = 1 
+# 기아닷컴의 트림/컬러/옵션/엑세사리 선택이 완료된 최종 견적 페이지에서 각 요소(세금) 선택에 따른 가격 계산 웹페이지 반영 대기 시간
+
+[EV6]
+NAME = #차량 이름
+NUM = #인승 (해당 요소가 없을 경우 none으로 기재)
+URL = # 기아닷컴 차량 견적내기 엔트리 페이지 (ex.https://www.kia.com/kr/buy/build-your-car/build/trim?rcCode=KRCV136)
+ENGINE = # 엔진 사양
+TRANSMISSION = #미션 사양
+TRIM = # 트림
+EXT_COLOR = # 외장 컬러
+INT_COLOR = # 내장 컬러
+PACKAGE = # 컬러/패키지 (해당 요소가 없을 경우 none으로 기재)
+OPTION = # 추가 상세 옵션
+
+...
+
+```
+
+차량의 각 정보는 기아닷컴의 태그 element 선택자로 기입해야된다.
+setting.ini의 값은 기아닷컴의 label 태그의 for 속성값 기준의 XPATH로 작동됨
+
+아래는 setting.ini Sorento 차량의 데이터 값 샘플
+
+
+| Key | Value |
+|:-----|:-----:|
+| NAME | Sorento |
+| NUM | filterItem_0_MQ-7 |
+| URL | https://www.kia.com/kr/buy/build-your-car/build/trim?rcCode=KRMQ135 |
+| ENGINE | filterItem_1_MQ-2 |
+| TRANSMISSION | filterItem_2_MQ-D |
+| TRIM | trimItem_1033 |
+| EXT_COLOR | colorChip_5_0 |
+| INT_COLOR | colorChip_7_0 |
+| PACKAGE | spec_options_0_A |
+| OPTION | detail_option_0_CLT |
+
+
+아래의 이미지의 붉은 상자처럼 차량의 원하는 옵션의 값을 기아닷컴에서 추출하면 된다.
+( 예 : 쏘렌토 차량(인승) -> label 태그의 for 값 : filterItem_0_MQ-7 )
+
+![image01](./img/image_01.png)
+
+### 2. 코드 추가
+
+`setting.ini`의 차량 추가가 완료되면 코드에서 해당 차량을 리스트에 추가해야 된다.
+
+```python 
+* priceCrawling.py *
+
+...
+
+## Read setting.ini
+properties = configparser.ConfigParser()
+properties.read('./setting.ini')
+
+default = properties["Default"]
+
+## 기존차량
+Spotage = properties["Spotage"]
+K8 = properties["K8"]
+
+##-------- 신규 차량(코드 추가 예시) --------
+Sorento = properties["Sorento"] # setting.ini에서 추가한 차량(Sorento) 정보 Read
+
+global RESULTPAGE_LOADTIME
+RESULTPAGE_LOADTIME = int(default["RESULTPAGE_LOADTIME"])
+
+##-------- 신규 차량(코드 추가 예시) --------
+#carList = [Spotage, K8] # 기존 차량 리스트
+carList = [Spotage, K8, Sorento] # 기존 리스트에 신규 차량(Sorento) 추가
+
+...
+
+```
+
+## 샘플 양식 & 크롤링 결과
+
+### 결과 추출을 위한 샘플 참고 양식
+![sample01](./img/sample_01.png)
+![sample02](./img/sample_02.png)
+
+### 크롤링 결과 EXCEL 파일
+
+![result01](./img/result_01.png)
---
+++ priceCrawling.py
@@ -0,0 +1,259 @@
+# #-*- coding: utf-8 -*-
+# ### 2024.02.16 
+# ### 작성자 : 유정원
+
+# pip install selenium
+# pip install webdriver-manager
+# pip install configparser
+# pip install openpyxl
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from openpyxl import Workbook
+from datetime import datetime
+import os
+import configparser
+import time
+import copy
+
+def pricePrint(row):
+    paymentPrice = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]/div[1]/div/div/div[4]/h3/button/span[2]/span[1]').text
+    paymentPrice = paymentPrice[:-1]
+    # paymentPrice = paymentPrice.replace(",", "")
+    row.append(paymentPrice)    
+    print("결제 금액 : " + paymentPrice)
+
+    taxPrice = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]/div[1]/div/div/div[5]/h3/button/span[2]/span[1]').text
+    taxPrice = taxPrice[:-1]
+    # taxPrice = taxPrice.replace(",", "")
+    row.append(taxPrice)
+    print("면세 구분 및 등록비 : " + taxPrice)
+
+    acquisitionPrice = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]/div[1]/div/div/div[5]/div/div[2]/div/div[2]').text
+    acquisitionPrice = acquisitionPrice[:-1]
+    # acquisitionPrice = acquisitionPrice.replace(",", "")
+    row.append(acquisitionPrice)
+    print("취득세 : " + acquisitionPrice)
+
+    bondPrice = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]/div[1]/div/div/div[5]/div/div[3]/div[1]/div[2]').text
+    bondPrice = bondPrice[:-1]
+    # bondPrice = bondPrice.replace(",", "")
+    row.append(bondPrice)
+    print("공채 : " + bondPrice)
+
+    licensePrice = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]/div[1]/div/div/div[5]/div/div[3]/div[3]/div[1]/div[2]').text
+    licensePrice = licensePrice[:-1]
+    # licensePrice = licensePrice.replace(",", "")
+    row.append(licensePrice)
+    print("차량 번호판 : " + licensePrice)
+
+    registPrice = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]/div[1]/div/div/div[5]/div/div[3]/div[3]/div[2]/div[2]').text
+    registPrice = registPrice[:-1]
+    # registPrice = registPrice.replace(",", "")
+    row.append(registPrice)
+    print("등록 대행 수수료 : " + registPrice)
+
+    try:
+        mTaxPrice = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]/div[1]/div/div/div[5]/div/div[4]/div[1]/div[2]').text
+        mTaxPrice = mTaxPrice[:-1]
+        # mTaxPrice = mTaxPrice.replace(",", "")
+        mTaxPrice = mTaxPrice.replace(" ", "")
+        row.append(mTaxPrice)
+        print("세금 변동 : " + mTaxPrice)
+    except:
+        row.append("-")
+        pass
+
+    print(row)
+    resultRows.append(row)
+
+
+def main(car):
+    row = []
+    trimSelected = ""
+    carName = car["NAME"]
+    row.append(carName)
+    print(carName)
+
+    # 크롤러 작동부
+    URL = car["URL"]
+    driver.get(URL)
+
+    driver.find_element(By.CLASS_NAME, 'conf-btn-close').click()
+    driver.implicitly_wait(3)
+
+    ## 인승 선택
+    peopleNum = str(car["NUM"])
+    if peopleNum == "none":
+        pass     
+    else : 
+        number = str("//label[@for='" + car["NUM"] + "']")
+        driver.find_element(By.XPATH, number).click()
+        trimSelected += driver.find_element(By.XPATH, number).text + "/"
+        driver.implicitly_wait(3)
+
+    ## 엔진 선택
+    engine = str("//label[@for='" + car["ENGINE"] + "']")
+    driver.find_element(By.XPATH, engine).click()
+    trimSelected += driver.find_element(By.XPATH, engine).text + " / "
+    driver.implicitly_wait(3)    
+
+    ## 미션 선택
+    trans = str("//label[@for='" + car["TRANSMISSION"] + "']")
+    driver.find_element(By.XPATH, trans).click()
+    trimSelected += driver.find_element(By.XPATH, trans).text + " / "
+    driver.implicitly_wait(3)    
+
+    ## 트림
+    trim = str("//label[@for='" + car["TRIM"] + "']")
+    driver.find_element(By.XPATH, trim).click()
+    trimSelected += driver.find_element(By.XPATH, trim+"/div/strong").text + " / "
+    driver.implicitly_wait(3)    
+
+    ## 컬러 선택 이동
+    driver.find_element(By.XPATH, '//*[@id="build_menu_btns"]/div/div/div/div/button').click()
+    driver.implicitly_wait(3)    
+
+    ## 외장 컬러 선택
+    extColor = str("//label[@for='" + car["EXT_COLOR"] + "']")
+    driver.find_element(By.XPATH, extColor).click()
+    trimSelected += driver.find_element(By.XPATH, '//*[@id="configurator_menu_content"]/div[1]/div/div/div/div[4]/div[1]/div[1]').text + " / "
+    driver.implicitly_wait(3)    
+
+    ## 내장 컬러 선택
+    intColor = str("//label[@for='" + car["INT_COLOR"] + "']")
+    driver.find_element(By.XPATH, intColor).click()
+    trimSelected += driver.find_element(By.XPATH, '//*[@id="configurator_menu_content"]/div[1]/div/div/div/div[6]/div[1]/div').text + " / "    
+    driver.implicitly_wait(3)    
+
+    ## 옵션 선택
+    driver.find_element(By.XPATH, '//*[@id="build_menu_btns"]/div/div/div/div[2]/button').click()
+    driver.implicitly_wait(3)
+
+    ## 패키지 선택
+    packageNum = str(car["PACKAGE"])
+    if packageNum == "none":
+        pass
+    else:
+        package = str("//label[@for='" + car["PACKAGE"] + "']")
+        driver.find_element(By.XPATH, package).click()
+        trimSelected += driver.find_element(By.XPATH, package).text + " / "
+        driver.implicitly_wait(3)        
+
+    ## 상세 옵션 선택
+    option = str("//label[@for='" + car["OPTION"] + "']")
+    driver.find_element(By.XPATH, option).click()
+    trimSelected += driver.find_element(By.XPATH, option + "/span[2]").text
+    driver.implicitly_wait(3)
+
+    ## 선택 내용 추가
+    row.append(trimSelected)   
+
+    ## 액세사리 선택(없음)
+    driver.find_element(By.XPATH, '//*[@id="build_menu_btns"]/div/div/div/div[2]/button').click()
+    driver.implicitly_wait(3)
+
+    ## 견적 완료
+    driver.find_element(By.XPATH, '//*[@id="build_menu_btns"]/div/div/div/div[2]/button').click()
+    time.sleep(3) #계산 때문에 3초 딜레이 추가
+
+    ## 면세 구분 클릭
+    button_element = driver.find_element(By.XPATH, '//button[@data-link-label="면세 구분 및 등록비_상세보기"]')
+    driver.execute_script("arguments[0].click();", button_element)
+    time.sleep(RESULTPAGE_LOADTIME)
+
+    ### 일반인
+    row.append("일반인")
+    pricePrint(row.copy())
+
+    ### 일반인 + 다자녀
+    row[2] = "일반인+다자녀"
+    checkBox = driver.find_element(By.XPATH, "//label[@for='multiChildYn']")
+    driver.execute_script("arguments[0].click();", checkBox)
+    time.sleep(RESULTPAGE_LOADTIME)
+    pricePrint(row.copy())
+
+    ## 면세선택
+    dropdown_button = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]/div[1]/div/div/div[5]/div/div[1]/div[2]/div[1]")
+    driver.execute_script("arguments[0].click();", dropdown_button)
+    time.sleep(RESULTPAGE_LOADTIME)
+
+    ### 장애 4~6 등급
+    row[2] = "장애 4~6 등급"
+    list01 = driver.find_element(By.XPATH, "//li[@class='dropdown-select__item']/button[text()='장애 4~6급']")
+    driver.execute_script("arguments[0].click();", list01)
+    time.sleep(RESULTPAGE_LOADTIME)
+    pricePrint(row.copy())
+
+    ## 국가유공자
+    row[2] = "국가유공자"
+    list02 = driver.find_element(By.XPATH, "//li[@class='dropdown-select__item']/button[text()='국가유공자']")
+    driver.execute_script("arguments[0].click();", list02)
+    time.sleep(RESULTPAGE_LOADTIME)
+    pricePrint(row.copy())
+
+    ## 광주민주화
+    row[2] = "광주민주화"
+    list03 = driver.find_element(By.XPATH, "//li[@class='dropdown-select__item']/button[text()='광주민주화']")
+    driver.execute_script("arguments[0].click();", list03)
+    time.sleep(RESULTPAGE_LOADTIME)
+    pricePrint(row.copy())
+
+    for eachRow in resultRows:
+        resultFile.append(eachRow)
+
+    wb.save(os.path.join(os.getcwd(), "result/") + "KiaPrice_" + currentTime+ ".xlsx")
+    print("---------------------------------------------")
+    time.sleep(0.5)
+    
+## Excel setting
+wb = Workbook()
+resultFile = wb.active
+resultRows = []
+currentTime = str(datetime.today().strftime("%Y%m%d_%H%M"))
+
+resultFile["A1"].value = "차량"
+resultFile["B1"].value = "선택 내용"
+resultFile["C1"].value = "면세 구분"
+resultFile["D1"].value = "결제 금액"
+resultFile["E1"].value = "면세 구분 및 등록비"
+resultFile["F1"].value = "취득세"
+resultFile["G1"].value = "공채"
+resultFile["H1"].value = "차량 번호판"
+resultFile["I1"].value = "등록 대행 수수료"
+resultFile["J1"].value = "세금 변동"
+
+## Read setting.ini
+properties = configparser.ConfigParser()
+properties.read('./setting.ini')
+
+default = properties["Default"]
+Spotage = properties["Spotage"]
+K8 = properties["K8"]
+Sorento = properties["Sorento"]
+
+global RESULTPAGE_LOADTIME
+RESULTPAGE_LOADTIME = int(default["RESULTPAGE_LOADTIME"])
+
+# carList = [Spotage, K8, Sorento]
+carList = [Spotage, K8, Sorento]
+
+## selenuim Chrome Driver Option Setting
+options = webdriver.ChromeOptions()
+options.add_argument('lang=ko_KR') # 사용언어 한국어
+# options.add_argument('disable-gpu') # 하드웨어 가속 안함
+# options.add_argument('log-level=3')
+options.add_argument('--window-size=1920,1080') # 해상도 설정 
+options.add_argument('headless')
+# options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+options.add_experimental_option("detach",True) # 웹브라우저 유지
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+for car in carList:
+    main(car)
+
+print("------------End------------")
